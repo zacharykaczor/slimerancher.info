6600bd4f5877ac3b5201f453f48ddfa4aba98085
Zachary <zachary@kaczor.us>
2022-09-24 21:19:58 -0400

    IDK I changed everything

--- index.js
+++ index.js
@@ -1,31 +1,95 @@
+const modal = document.querySelector("#information");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const information = document.querySelector("#selected");
 
-const mapTexture = new Image();
-mapTexture.src = "map.png";
+const mapImage = new Image();
+mapImage.src = "./map.webp";
 
-const podTexture = new Image();
-podTexture.src = "pod.png";
+const podImage = new Image();
+podImage.src = "./pod.webp";
+
+var mapTexture = null;
+var podTexture = null;
 
 import markers from "./markers.js";
 
-var origin = {
-    x: 0,
-    y: 0
+class Point {
+    x;
+    y;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    distance(other) {
+        var a = this.x - other.x;
+        var b = this.y - other.y;
+
+        return Math.sqrt(a ** 2 + b ** 2);
+    }
 }
 
-var mouse = {
-    latestDown: null,
-    visible: false,
-    x: 0,
-    y: 0
+class Camera {
+    position = new Point(0, 0)
+
+    zoomFactor = 1.2
+    zoom = 1
+
+    zoomAt(at, amount) {
+        this.position.x = at.x - (at.x - this.position.x) * amount;
+        this.position.y = at.y - (at.y - this.position.y) * amount;
+        this.zoom *= amount;
+    }
+
+    zoomIn(at) {
+        let maxZoom = 5;
+
+        if (this.zoom * this.zoomFactor < maxZoom) {
+            this.zoomAt(at, this.zoomFactor);
+        }
+    }
+
+    zoomOut(at) {
+        let minZoom = Math.max(
+            canvas.width / mapTexture.width, 
+            canvas.height / mapTexture.height
+        );
+
+        if (this.zoom / this.zoomFactor > minZoom) {
+            this.zoomAt(at, 1 / this.zoomFactor);
+        } else {
+            this.zoom = minZoom;
+        }
+    }
 }
 
-var panning = false;
-var scale = 1;
+// Set origin to either centered around the conservatory or centered in general and zoomed out.
+const camera = new Camera();
+
+function clamp(min, value, max) {
+    return Math.max(min, Math.min(value, max));
+}
+
+// Todo: Make these use Point.
+function toWorld(x, y) {
+    x = (x - camera.position.x) / camera.zoom;
+    y = (y - camera.position.y) / camera.zoom;
+    return new Point(x, y);
+}
+
+function toScreen(x, y) {
+    x = x * camera.zoom + camera.position.x;
+    y = y * camera.zoom + camera.position.y;
+    return {x, y};
+}
 
 function render() {
+    // Todo: Clean up this function.
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+
+    // This camera work should be moved to the class.
     let one = toScreen(mapTexture.width, mapTexture.height);
 
     let two = toWorld(
@@ -34,20 +98,20 @@ function render() {
     )
 
     let three = {
-        x: -two.x * scale,
-        y: -two.y * scale
+        x: -two.x * camera.zoom,
+        y: -two.y * camera.zoom
     }
 
-    origin.x = clamp(three.x, origin.x, 0);
-    origin.y = clamp(three.y, origin.y, 0);
+    camera.position.x = clamp(three.x, camera.position.x, 0);
+    camera.position.y = clamp(three.y, camera.position.y, 0);
 
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.setTransform(scale, 0, 0, scale, origin.x, origin.y);
+    ctx.setTransform(camera.zoom, 0, 0, camera.zoom, camera.position.x, camera.position.y);
     ctx.drawImage(mapTexture, 0, 0);
 
     for (let marker of markers) {
-        let markerSize = 50 / scale;
+        let markerSize = 50 / camera.zoom;
 
         ctx.drawImage(
             podTexture, 
@@ -57,142 +121,108 @@ function render() {
             markerSize
         );
 
-        ctx.font = `bold ${ 25 / scale }px monospace`;
+        ctx.font = `bold ${ 25 / camera.zoom }px monospace`;
         ctx.textBaseline = "middle";
         ctx.textAlign = "left";
         ctx.fillText(marker.reward, marker.x + markerSize / 1.5, marker.y);
     }
-
-    if (mouse.visible) {
-        let mouseInWorld = toWorld(mouse.x, mouse.y);
-
-        mouseInWorld.x = Math.round(mouseInWorld.x);
-        mouseInWorld.y = Math.round(mouseInWorld.y);
     
-        ctx.textAlign = "right";
-        ctx.textBaseline = "top";
-        ctx.fillText(mouseInWorld.x + " " + mouseInWorld.y, mouseInWorld.x - 10, mouseInWorld.y)
+    requestAnimationFrame(render)
+}
+
+async function load() {
+    // Todo: Add loading circle or something
+    mapTexture = await createImageBitmap(mapImage);
+    podTexture = await createImageBitmap(podImage);
+
+    requestAnimationFrame(render);
+}
+
+window.addEventListener("load", load);
+
+var panning = false;
+
+const startPanning = () => panning = true;
+const stopPanning = () => panning = false;
+const hideModal = () => {
+    information.innerHTML = null;
+    information.classList.add("hidden");
+}
+canvas.addEventListener("mousedown", function() {
+    hideModal();
+    startPanning();
+});
+
+canvas.addEventListener("mouseleave", stopPanning);
+canvas.addEventListener("mouseup", stopPanning);
+
+canvas.addEventListener("wheel", function(event) {
+    hideModal();
+    let at = {
+        x: event.clientX,
+        y: event.clientY
     }
-}
 
-function resize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    render();
-}
-
-window.addEventListener("load", resize);
-window.addEventListener("resize", resize);
-function scaleAt(x, y, scaleBy) {
-    origin.x = x - (x - origin.x) * scaleBy;
-    origin.y = y - (y - origin.y) * scaleBy;
-    scale *= scaleBy;
-}
-
-window.addEventListener("wheel", function(event) {
-    var scaleBy = 1.2;
-
-    if (event.deltaY < 0 && scale < 4) {
-        scaleAt(event.clientX, event.clientY, scaleBy);
+    if (event.deltaY < 0) {
+        camera.zoomIn(at);
     }
 
     if (event.deltaY > 0) {
-        let minimumScale = canvas.width / mapTexture.width;
-        scaleBy = 1 / scaleBy;
-
-        if (scale * scaleBy < minimumScale) {
-            scale = minimumScale;
-        } else {
-            scaleAt(event.clientX, event.clientY, scaleBy)
-        }
+        camera.zoomOut(at);
     }
-
-    render();
 });
 
-function clamp(min, value, max) {
-    return Math.max(min, Math.min(value, max));
+function getMarkerAtCursor(mousePosition) {
+    var clickDistance = 50 / 2 / camera.zoom;
+    mousePosition = toWorld(mousePosition.x, mousePosition.y);
+
+    for (let marker of markers) {
+        let position = new Point(marker.x, marker.y);
+
+        if (mousePosition.distance(position) < clickDistance) {
+            return marker;
+        }
+    }
 }
 
-function toWorld(x, y) {
-    x = (x - origin.x) / scale;
-    y = (y - origin.y) / scale;
-    return {x, y};
-}
+canvas.addEventListener("click", function(event) {
+    let clickedMarker = getMarkerAtCursor(new Point(event.clientX, event.clientY));
 
-function toScreen(x, y) {
-    x = x * scale + origin.x;
-    y = y * scale + origin.y;
-    return {x, y};
-}
+    if (clickedMarker) {
+        information.innerHTML = `
+        <p>
+            <b>${clickedMarker.reward}</b>
+        </p>
 
-function distance(p1, p2) {
-    var a = p1.x - p2.x;
-    var b = p1.y - p2.y;
-    
-    return Math.sqrt(a * a + b * b);
-}
+        <p>
+            ${clickedMarker.notes}
+        </p>
 
-canvas.addEventListener("mousedown", function(event) {
-    mouse.latestDown = event;
-    panning = true;
-})
+        <p>
+            <b>X:</b> ${clickedMarker.x}
+            <br>
+            <b>Y:</b> ${clickedMarker.y}
+        </p>
+        `;
+
+        information.classList.remove("hidden");
+
+        let markerOnScreen = toScreen(clickedMarker.x, clickedMarker.y);
+
+        if (information.clientHeight - markerOnScreen.y + 50 < 0) {
+            information.style.top = (markerOnScreen.y - information.clientHeight - 50) + "px";
+        } else {
+            information.style.top = markerOnScreen.y + 50 + "px";
+        }
+
+        information.style.left = (markerOnScreen.x - information.clientWidth / 2) + "px";
+
+    }
+});
 
 canvas.addEventListener("mousemove", function(event) {
     if (panning) {
-        origin.x += event.movementX;
-        origin.y += event.movementY;
+        camera.position.x += event.movementX;
+        camera.position.y += event.movementY;
     }
-
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
-
-    render();
-})
-
-canvas.addEventListener("mouseup", function(event) {
-    if (mouse.latestDown && 
-        event.clientX == mouse.latestDown.clientX &&
-        event.clientY == mouse.latestDown.clientY) 
-    {
-        var worldPosition = toWorld(event.clientX, event.clientY);
-        var clickedPod = false;
-    
-        for (let marker of markers) {
-            if (distance(worldPosition, marker) < 50 / 2 / scale) {
-                information.classList.remove("hidden")
-                information.innerHTML = `
-                    <b>${marker.reward}</b>
-                    <br>
-                    <p>${marker.notes}</p>
-                    <br>
-                    <p>
-                        X: ${marker.x}
-                        <br>
-                        Y: ${marker.y}
-                    </p>
-                `;
-    
-                clickedPod = true;
-                break;
-            }
-        }
-    
-        if (!clickedPod) {
-            information.classList.add("hidden")
-        }
-    }
-
-    panning = false;
 });
-
-canvas.addEventListener("mouseenter", function(event) {
-    mouse.visible = true;
-    render();
-});
-
-canvas.addEventListener("mouseleave", function(event) {
-    panning = false;
-    mouse.visible = false;
-    render();
-});;
