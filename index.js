b050e17ebb7f0b6209c1d20710f28c6f5532017e
Zachary <zachary@kaczor.us>
2022-09-26 17:11:00 -0400

    Added Research Drones, the ability to toggle Research Drones and Treasure Pods, moved the credits to a modal and added credits for the drones, made names only appear at 2x min zoom

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-const information = document.querySelector("#information");
-const coordinates = document.querySelector("#coordinates");
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+import { treasurePods, researchDrones } from "./markers.js";
+
+const creditsModal = document.querySelector("#credits-modal");
+const footer = document.querySelector("footer");
+const showCreditsButton = document.querySelector("#show-credits-button");
+const hideCreditsButton = document.querySelector("#hide-credits-button");
+const treasurePodsButton = document.querySelector("#treasure-pods-button");
+const researchDronesButton = document.querySelector("#research-drones-button");
+
+var showTreasurePods = true;
+var showResearchDrones = true;
+
+showCreditsButton.addEventListener("click", function() {
+    creditsModal.classList.remove("hidden");
+    footer.classList.add("hidden")
+});
+
+hideCreditsButton.addEventListener("click", function() {
+    creditsModal.classList.add("hidden");
+    footer.classList.remove("hidden")
+});
+
+function updateTreasurePodsButton() {
+    treasurePodsButton.children[0].textContent = showTreasurePods ? "Disable" : "Enable";
+}
+
+treasurePodsButton.addEventListener("click", function() {
+    showTreasurePods = !showTreasurePods;
+    updateTreasurePodsButton();
+});
+
+updateTreasurePodsButton();
+
+function updateResearchDronesButton() {
+    researchDronesButton.children[0].textContent = showResearchDrones ? "Disable" : "Enable";
+}
+
+researchDronesButton.addEventListener("click", function() {
+    showResearchDrones = !showResearchDrones;
+    updateResearchDronesButton();
+});
+
+updateResearchDronesButton();
 
 const mapImage = new Image();
 mapImage.src = "./map.webp";
@@ -9,11 +48,39 @@ mapImage.src = "./map.webp";
 const podImage = new Image();
 podImage.src = "./pod.webp";
 
+const droneImage = new Image();
+droneImage.src = "./drone.png";
+
 var mapTexture = null;
 var podTexture = null;
+var droneTexture = null;
 
-import markers from "./markers.js";
+function getVisibleMarkers() {
+    let visibleMarkers = [];
 
+    if (showTreasurePods) {
+        for (let treasurePod of treasurePods) {
+            treasurePod.icon = podTexture;
+        }
+
+        visibleMarkers.push(...treasurePods);
+    }
+
+    if (showResearchDrones) {
+        for (let researchDrone of researchDrones) {
+            researchDrone.icon = droneTexture;
+        }
+
+        visibleMarkers.push(...researchDrones);
+    }
+
+    return visibleMarkers;
+}
+
+const information = document.querySelector("#information");
+const coordinates = document.querySelector("#coordinates");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 class Point {
     x;
     y;
@@ -32,10 +99,11 @@ class Point {
 }
 
 class Camera {
-    position = new Point(0, 0)
+    position = new Point(0, 0);
 
-    zoomFactor = 1.2
-    zoom = 1
+    firstRender = true;
+    zoomFactor = 1.2;
+    zoom = 1;
 
     zoomAt(at, amount) {
         this.position.x = at.x - (at.x - this.position.x) * amount;
@@ -43,29 +111,39 @@ class Camera {
         this.zoom *= amount;
     }
 
-    zoomIn(at) {
-        let maxZoom = 5;
+    get maxZoom() {
+        return 5;
+    }
 
-        if (this.zoom * this.zoomFactor < maxZoom) {
+    zoomIn(at) {
+        if (this.zoom * this.zoomFactor < this.maxZoom) {
             this.zoomAt(at, this.zoomFactor);
         }
     }
 
-    zoomOut(at) {
-        let minZoom = Math.max(
+    get minZoom() {
+        return Math.max(
             canvas.width / mapTexture.width, 
             canvas.height / mapTexture.height
         );
+    }
 
-        if (this.zoom / this.zoomFactor > minZoom) {
+    enactMinZoom() {
+        if (this.zoom < this.minZoom) {
+            this.zoom = this.minZoom;
+        }
+    }
+
+    zoomOut(at) {
+        if (this.zoom / this.zoomFactor > this.minZoom) {
             this.zoomAt(at, 1 / this.zoomFactor);
         } else {
-            this.zoom = minZoom;
+            this.zoom = this.minZoom;
         }
     }
 }
 
-// Set origin to either centered around the conservatory or centered in general and zoomed out.
+// TODO: Set origin to either centered around the conservatory or centered in general and zoomed out.
 const camera = new Camera();
 
 function clamp(min, value, max) {
@@ -90,6 +168,17 @@ function render() {
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
 
+    camera.enactMinZoom();
+
+    if (camera.firstRender) {
+        camera.zoom = camera.minZoom;
+
+        camera.position.x = 0;
+        camera.position.y = 0;   
+
+        camera.firstRender = false;
+    }
+
     // This camera work should be moved to the class.
     let one = toScreen(mapTexture.width, mapTexture.height);
 
@@ -111,21 +200,23 @@ function render() {
     ctx.setTransform(camera.zoom, 0, 0, camera.zoom, camera.position.x, camera.position.y);
     ctx.drawImage(mapTexture, 0, 0);
 
-    for (let marker of markers) {
+    for (let marker of getVisibleMarkers()) {
         let markerSize = 50 / camera.zoom;
 
         ctx.drawImage(
-            podTexture, 
+            marker.icon, 
             marker.x - (markerSize / 2), 
             marker.y - (markerSize / 2), 
             markerSize, 
             markerSize
         );
 
-        ctx.font = `bold ${ 25 / camera.zoom }px monospace`;
-        ctx.textBaseline = "middle";
-        ctx.textAlign = "left";
-        ctx.fillText(marker.reward, marker.x + markerSize / 1.5, marker.y);
+        if (camera.zoom > 2 * camera.minZoom) {
+            ctx.font = `bold ${ 25 / camera.zoom }px monospace`;
+            ctx.textBaseline = "middle";
+            ctx.textAlign = "left";
+            ctx.fillText(marker.name, marker.x + markerSize / 1.5, marker.y);
+        }
     }
     
     requestAnimationFrame(render)
@@ -135,6 +226,7 @@ async function load() {
     // Todo: Add loading circle or something
     mapTexture = await createImageBitmap(mapImage);
     podTexture = await createImageBitmap(podImage);
+    droneTexture = await createImageBitmap(droneImage);
 
     requestAnimationFrame(render);
 }
@@ -180,7 +272,7 @@ function getMarkerAtCursor(mousePosition) {
     var clickDistance = 50 / 2 / camera.zoom;
     mousePosition = toWorld(mousePosition.x, mousePosition.y);
 
-    for (let marker of markers) {
+    for (let marker of getVisibleMarkers()) {
         let position = new Point(marker.x, marker.y);
 
         if (mousePosition.distance(position) < clickDistance) {
@@ -195,7 +287,7 @@ canvas.addEventListener("click", function(event) {
     if (clickedMarker) {
         information.innerHTML = `
         <p>
-            <b>${clickedMarker.reward}</b>
+            <b>${clickedMarker.name}</b>
         </p>
 
         <p>
